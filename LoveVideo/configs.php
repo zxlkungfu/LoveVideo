100a1b6e088836be89872e725fc4f79cbac55fdc
zxlkungfu <zxlkungfu@126.com>
2017-05-23 19:37:47 +0800

    Initial commit.
    Signed-off-by: zxlkungfu <zxlkungfu@126.com>

--- LoveVideo/configs.php
+++ LoveVideo/configs.php
@@ -4,6 +4,6 @@ define("DB_HOST", "localhost");
 define("DB_PORT", "8889");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "root");
-define("DB_DBNAME", "LoveVideo");
+define("DB_NAME", "LoveVideo");
 define("DB_CHARSET", "utf-8");
 
